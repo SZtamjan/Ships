bb0ccfb226c235a8c25779b80985f57203ee28f4
nrtqq <nrtqqgaming@gmail.com>
2022-11-19 12:23:36 +0100

    playMenu
    +playMenu
    +buttons
    + beta mainMenu
    + scene menegment

---
+++ Ships/Assets/Scripts/sceneManagement.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneManagement : MonoBehaviour
+{
+
+    public void changeScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
